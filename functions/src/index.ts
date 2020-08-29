64b1f6d569cc12659415b954b01c86417ffe484b
Kieren <kieren.pinto@hotmail.com>
2020-08-29 18:40:26 +1000

    Initial Commit

---
+++ functions/src/index.ts
@@ -0,0 +1,17 @@
+// import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+/**
+ * Import all request Types
+ */
+import {subjectRequest} from './subject';
+
+/** Export all request types */
+export {subjectRequest} 
