59536cea6a0e98effe54f39736bfba5ffe26012f
Kieren <kieren.pinto@hotmail.com>
2020-08-30 16:51:56 +1000

    users separated by class

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,18 +1,21 @@
 
+import * as functions from 'firebase-functions';
+import {makeRef} from './dbRefs'
+import { auth } from "firebase-admin";
 
 enum UserType {
-    Teacher,
-    Student,
-    Other
+    Teacher = 'Teacher',
+    Student = "Student",
+    Other = "Other"
 }
 
 class User {
-    constructor(public uid: string, public name: string, public email: string, public type: UserType) { }
+    constructor(public uid: string, public type: UserType) { }
 }
 
 const userConverter = {
     toFirestore(user: User): FirebaseFirestore.DocumentData {
-        return { uid: user.uid, name: user.name, email: user.email };
+        return { uid: user.uid, type: UserType};
     },
     fromFirestore(
         snapshot: FirebaseFirestore.QueryDocumentSnapshot
@@ -31,11 +34,56 @@ const userConverter = {
             }
         }
 
-        return new User(data.uid, data.name, data.email, findType());
+        return new User(data.uid, findType());
     }
 };
 
+/**
+ * This function handles create update and delete actions. Reads are done directly from firestore by the client.
+ */
+const userRequest = functions.https.onCall((data,context)=>{
+    const uid = context.auth?.uid
+    context.auth?.token.firebase.sign_in_provider
+    if (!uid){
+        throw new functions.https.HttpsError('permission-denied', "Request has not been authenticated");
+    }
+    switch (data.action) {
+        case 'create':
+            return create(data,uid);
+        case 'changeName':
+            return changeName(data.newName, uid);
+        case 'delete':
+            return remove(uid);
+        default:
+            // Return an error
+            throw new functions.https.HttpsError('invalid-argument',"Invalid subject action request");
+    }
+})
+
+async function create(data:any, uid: string){
+    const userType  = data.userType
+    const newUser = new User(uid,userType);
+    await makeRef.users.withConverter(userConverter).doc(uid).set(newUser);
+    return {response:'success'}
+}
+
+async function changeName(newName:string, uid:string){
+    // Change Name in Google Auth
+    await auth().updateUser(uid, {
+        displayName: newName
+    })
+    // Update name in each of the subjects if a teacher, or each of the questions if a student.
+    console.error("Have not implemented propagation of name update");
+
+    return {response:'success'}
+}
+
+async function remove(uid:string){
+    await auth().deleteUser(uid);
+    await makeRef.users.doc(uid).delete()
+    return {response:'success'}
+}
 
 
-export {User,userConverter, UserType};
+export {User,userConverter, UserType, userRequest};
 
